91e6dbc39283aba8fd4370c177b27956e2a0a3b1
HaiTran1880 <hai98988989@gmail.com>
2021-06-07 08:43:50 +0700

    7/6//21_Hai

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import R from '../../assets/R';
 import HomeView from './HomeView';
 import {
