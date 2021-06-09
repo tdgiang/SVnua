b2075ddbe8bdba1813c97143fdf4097c5c77d74b
Giang Tran <giangtd@dcv.vn>
2021-06-09 16:56:17 +0700

    commit

--- index.js
+++ index.js
@@ -2,15 +2,11 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 import SocketIO from './src/helper/SocketIO';
 import Room from './src/helper/Room';
 
-<<<<<<< HEAD
 AppRegistry.registerComponent(appName, () => App);
-=======
-AppRegistry.registerComponent(appName, () => Room);
->>>>>>> e9d70a7fc9c19750dcd151e0d0f880b0d253c725
